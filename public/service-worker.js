5606fb0a7a9bc91d9f67fe2a45f3761b02df3343
himatecorp2025 <himatecorp2025@gmail.com>
2026-07-14 17:50:56 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="klavierhaus-shell-v10";
+const CACHE_NAME="klavierhaus-shell-v11";
 const APP_SHELL=["/","/index.html","/styles.css","/app.js","/icons/icon-192.png","/icons/icon-512.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
